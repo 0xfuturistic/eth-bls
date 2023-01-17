88e498d50196f7a9b88f6d1dffb4b1b2abf29eec
diego <diego@gradient.city>
2023-01-17 22:23:50 +0100

    first commit & install succinctlabs/eth-proof-of-consensus

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
+import "eth-proof-of-consensus/contracts/src/lightclient/BLSAggregatedSignatureVerifier.sol";
 
 contract Counter {
     uint256 public number;
